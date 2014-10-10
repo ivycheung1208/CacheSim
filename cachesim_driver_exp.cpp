a35c99fe6c0b27df17b23f47fabbd480f778dfc7
ivycheung1208 <ivycheung1208@hotmail.com>
2014-10-10 03:26:08 -0400

    Driver experiment
    Defined constATT_MAX here instead of in cachesim.cpp for better function
    isolation.

--- cachesim_driver_exp.cpp
+++ cachesim_driver_exp.cpp
@@ -1,77 +1,69 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-//#include <unistd.h>
+// include this line if you are running under Unix environment
+// #include <unistd.h>
+// include this line if you are running under Windows environment
 #include "XGetopt.h"
 #include "cachesim.hpp"
 
+static const double AAT_MAX = 1000;
+
 void print_help_and_exit(void) {
-    printf("cachesim [OPTIONS] < traces/file.trace\n");
-    printf("  -c C\t\tTotal size in bytes is 2^C\n");
-    printf("  -b B\t\tSize of each block in bytes is 2^B\n");
-    printf("  -s S\t\tNumber of blocks per set is 2^S\n");
-    printf("  -v V\t\tNumber of blocks in victim cache\n");
-    printf("  -k K\t\tPrefetch Distance");
+	printf("cachesim [OPTIONS] < traces/file.trace\n");
+	printf("  -c C\t\tTotal size in bytes is 2^C\n");
+	printf("  -b B\t\tSize of each block in bytes is 2^B\n");
+	printf("  -s S\t\tNumber of blocks per set is 2^S\n");
+	printf("  -v V\t\tNumber of blocks in victim cache\n");
+	printf("  -k K\t\tPrefetch Distance");
 	printf("  -h\t\tThis helpful output\n");
-    exit(0);
+	exit(0);
 }
 
 void print_statistics(cache_stats_t* p_stats);
 
 int main(int argc, char* argv[]) {
-	//uint64_t test;
-	//test = (1 << 3) - 1;
-	//printf("%" PRIu64 "\n", test);
-    int opt;
-    uint64_t c = DEFAULT_C;
-    uint64_t b = DEFAULT_B;
-    uint64_t s = DEFAULT_S;
-    uint64_t v = DEFAULT_V;
+	int opt;
+	uint64_t c = DEFAULT_C;
+	uint64_t b = DEFAULT_B;
+	uint64_t s = DEFAULT_S;
+	uint64_t v = DEFAULT_V;
 	uint64_t k = DEFAULT_K;
-    FILE* fin  = stdin;
+	FILE* fin  = stdin;
 	FILE* fout = stdout;
 	char inputfile[100];
 	char outputfile[100];
 
-    /* Read arguments */ 
-    while(-1 != (opt = getopt(argc, argv, "c:b:s:i:v:k:h"))) {
-        switch(opt) {
+	/* Read arguments */ 
+	while(-1 != (opt = getopt(argc, argv, "c:b:s:i:v:k:h"))) {
+		switch(opt) {
 		case 'i':
 			strcpy(inputfile, optarg);
 			strcpy(outputfile, optarg);
 			strcat(outputfile, ".out");
 			break;
 		case 'c':
-            c = atoi(optarg);
-            break;
-        case 'b':
-            b = atoi(optarg);
-            break;
-        case 's':
-            s = atoi(optarg);
-            break;
-        case 'v':
-            v = atoi(optarg);
-            break;
+			c = atoi(optarg);
+			break;
+		case 'b':
+			b = atoi(optarg);
+			break;
+		case 's':
+			s = atoi(optarg);
+			break;
+		case 'v':
+			v = atoi(optarg);
+			break;
 		case 'k':
 			k = atoi(optarg);
 			break;
-        case 'h':
-            /* Fall through */
-        default:
-            print_help_and_exit();
-            break;
-        }
-    }
-
-/*	printf("Cache Settings\n");
-	printf("C: %" PRIu64 "\n", c);
-	printf("B: %" PRIu64 "\n", b);
-	printf("S: %" PRIu64 "\n", s);
-	printf("V: %" PRIu64 "\n", v);
-	printf("K: %" PRIu64 "\n", k);
-	printf("\n");
-*/
+		case 'h':
+			/* Fall through */
+		default:
+			print_help_and_exit();
+			break;
+		}
+	}
 
 	fout = fopen(outputfile, "w");
 	fprintf(fout, "%s:\n\n", inputfile);
@@ -164,21 +156,22 @@ int main(int argc, char* argv[]) {
 void print_statistics(cache_stats_t* p_stats) {
 	printf("\n");
 	printf("Cache Statistics\n");
-    printf("Accesses: %" PRIu64 "\n", p_stats->accesses);
-    printf("Reads: %" PRIu64 "\n", p_stats->reads);
-    printf("Read misses: %" PRIu64 "\n", p_stats->read_misses);
-    printf("Read misses combined: %" PRIu64 "\n", p_stats->read_misses_combined);
-    printf("Writes: %" PRIu64 "\n", p_stats->writes);
-    printf("Write misses: %" PRIu64 "\n", p_stats->write_misses);
-    printf("Write misses combined: %" PRIu64 "\n", p_stats->write_misses_combined);
-    printf("Misses: %" PRIu64 "\n", p_stats->misses);
-    printf("Writebacks: %" PRIu64 "\n", p_stats->write_backs);
+	printf("Accesses: %" PRIu64 "\n", p_stats->accesses);
+	printf("Reads: %" PRIu64 "\n", p_stats->reads);
+	printf("Read misses: %" PRIu64 "\n", p_stats->read_misses);
+	printf("Read misses combined: %" PRIu64 "\n", p_stats->read_misses_combined);
+	printf("Writes: %" PRIu64 "\n", p_stats->writes);
+	printf("Write misses: %" PRIu64 "\n", p_stats->write_misses);
+	printf("Write misses combined: %" PRIu64 "\n", p_stats->write_misses_combined);
+	printf("Misses: %" PRIu64 "\n", p_stats->misses);
+	printf("Writebacks: %" PRIu64 "\n", p_stats->write_backs);
 	printf("Victim cache misses: %" PRIu64 "\n", p_stats->vc_misses);
 	printf("Prefetched blocks: %" PRIu64 "\n", p_stats->prefetched_blocks);
 	printf("Useful prefetches: %" PRIu64 "\n", p_stats->useful_prefetches);
 	printf("Bytes transferred to/from memory: %" PRIu64 "\n", p_stats->bytes_transferred);
 	printf("Hit Time: %f\n", p_stats->hit_time);
-    printf("Miss Penalty: %" PRIu64 "\n", p_stats->miss_penalty);
-    printf("Miss rate: %f\n", p_stats->miss_rate);
+	printf("Miss Penalty: %" PRIu64 "\n", p_stats->miss_penalty);
+	printf("Miss rate: %f\n", p_stats->miss_rate);
 	printf("Average access time (AAT): %f\n", p_stats->avg_access_time);
 }
+
